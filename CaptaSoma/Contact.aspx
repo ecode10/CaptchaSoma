69981a6d15fd12293167faf4bdc45a39066d25f2
Mauricio Junior <mauricio.junior@compline.com.br>
2019-01-28 19:38:58 +0000

    captcha
    captcha .net

---
+++ CaptaSoma/Contact.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="CaptaSoma.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Contato</h3>
+
+    <asp:TextBox runat="server" ID="txtNome" placeholder="Digite o nome" />
+
+    <br />
+
+    <asp:TextBox runat="server" ID="txtEmail" placeholder="Digite o e-mail" />
+
+    <br />
+
+    <asp:TextBox runat="server" ID="txtDescricao" placeholder="Digite uma descrição" />
+
+    <br />
+    <asp:Label Text="" ID="lblSoma" runat="server" /> <asp:TextBox runat="server" ID="txtSoma" />
+
+    <asp:Button Text="Enviar" ID="cmdEnviar" runat="server" OnClick="cmdEnviar_Click"
+        />
+    <asp:Label Text="" ID="lblResultado" ForeColor="Red" runat="server" />
+
+</asp:Content>
---
+++ CaptaSoma/Contact.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CaptaSoma
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                RandomSoma();
+            }
+        }
+
+        private void RandomSoma()
+        {
+            Random random = new Random();
+
+            var _primeiroValor = random.Next(0, 10);
+            var _segundoValor = random.Next(5, 15);
+
+            lblSoma.Text = _primeiroValor + "+" + _segundoValor;
+        }
+
+        protected void cmdEnviar_Click(object sender, EventArgs e)
+        {
+            var _soma = lblSoma.Text.Split('+');
+            var _primeiroValor = _soma[0];
+            var _segundoValor = _soma[1];
+
+            var _somaTotal = int.Parse(_primeiroValor) +
+                int.Parse(_segundoValor);
+
+            if (!txtSoma.Text.Equals(_somaTotal.ToString()))
+            {
+                lblResultado.Text = "Wrong, try again";
+                return;
+            }
+
+            string _nome = txtNome.Text;
+            string _email = txtEmail.Text;
+            string _descricao = txtDescricao.Text;
+
+            enviarEmail(_nome, _email, _descricao);
+            limpaCampos();
+        }
+
+        private void limpaCampos()
+        {
+            lblResultado.Text = "Email enviado com sucesso.";
+            txtDescricao.Text = "";
+            txtEmail.Text = "";
+            txtNome.Text = "";
+        }
+
+        #region "Envia email "
+        private void enviarEmail(string nome, string email, string descricao)
+        {
+            
+        }
+        #endregion
+    }
+}
